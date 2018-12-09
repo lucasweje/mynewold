268b9e0ed57e6b31cf959b2ddf5371ef950be6cd
luwe16ab@student.cbs.dk <lucasweje@gmail.com>
2018-12-09 13:24:03 +0100

    AddItemScreen fixed så sælgeren at tøjet bliver sat lig med currenuser der er logget ind

--- components/pages/AddItemScreen.js
+++ components/pages/AddItemScreen.js
@@ -28,32 +28,28 @@ export default class AddItemScreen extends React.Component {
     title: "Add item"
   };
 
+  componentDidMount(){
+    this.getSellerData();
+
+  }
+
 
   addItemToDatabase() {
 
-
-    const title = this.state.title;
-    const brand = this.state.brand;
-    const category = this.state.category;
-    const color = this.state.color;
-    const size = this.state.size;
-    const description = this.state.description;
-    const image = firebase.auth().currentUser.uid + title;
-    const price = this.state.price;
-    const result = this.state.result;
+    var title = this.state.title;
+    var brand = this.state.brand;
+    var category = this.state.category;
+    var color = this.state.color;
+    var size = this.state.size;
+    var description = this.state.description;
+    var image = firebase.auth().currentUser.uid + title;
+    var price = this.state.price;
+    var result = this.state.result;
+    var seller = this.state.seller;
 
     // Laver variabel 'that' til at holde 'this' da vi arbejder inde i et Firebase kald.
-    var that = this;
+    var that = this;  
 
-    // tager currentUser for at finde kunne sætte 'seller' = den person som er logget ind
-    firebase.database().ref('users/' + firebase.auth().currentUser.uid).on('value', function (snapshot) {
-      profileObject = snapshot.val();
-      that.setState({
-        seller: profileObject.firstName + " " + profileObject.lastName
-      });
-    });
-
-    const seller = this.state.seller;
 
     // Tjekker først at kateogiren er valgt fra 'Pickeren'
     if (category) {
@@ -105,9 +101,17 @@ export default class AddItemScreen extends React.Component {
   }
 
   getSellerData() {
-    
+    var that = this;
+     // tager currentUser for at finde kunne sætte 'seller' = den person som er logget ind
+     firebase.database().ref('users/' + firebase.auth().currentUser.uid).on('value', function (snapshot) {
+      profileObject = snapshot.val();
+      that.setState({
+        seller: profileObject.firstName + " " + profileObject.lastName
+      });
 
-  };
+    });
+
+  }
 
   onChooseImagePress = async () => {
     await Permissions.askAsync(Permissions.CAMERA);
@@ -132,6 +136,7 @@ export default class AddItemScreen extends React.Component {
 
   render() {
 
+
     if (this.state.isLoading) {
       return (
         <View style={{ flex: 1, padding: 20, justifyContent: 'center', alignItems: 'center' }}>
@@ -144,99 +149,101 @@ export default class AddItemScreen extends React.Component {
 
     return (
 
-      <ScrollView contentContainerStyle={styles.container} pagingEnabled={true}>
+      <View style={styles.container}>
 
-          <View style={{ height: 50, marginBottom: 15, }}>
-            <Text style={{ alignSelf: "center" }}>Title of product:</Text>
-            <TextInput
-              label='Title'
-              // placeholder='title of product'
-              value={this.state.title}
-              onChangeText={title => this.setState({ title })}
-              style={styles.textInputBorder}
-              underlineColorAndroid='transparent'
-            />
-          </View>
 
-          <View style={{ height: 50, marginBottom: 15, }}>
-            <Text style={{ alignSelf: "center" }}>Brand:</Text>
-            <TextInput
-              label='brand'
-              // placeholder='brand of product'
-              value={this.state.brand}
-              onChangeText={brand => this.setState({ brand })}
-              style={styles.textInputBorder}
-              underlineColorAndroid='transparent'
-            />
-          </View>
 
-          <View style={{ height: 50, marginBottom: 0, }}>
-            <Text style={{ alignSelf: "center" }}>Category:</Text>
-            {/* En "dropdown" picker der kun giver brugere mulighed for at vælge de kategorier vi har oprettet */}
-            <Picker
-              selectedValue={this.state.category}
-              onValueChange={itemValue => this.setState({ category: itemValue })}
-              style={{ marginTop: -15 }}
-            >
-              <Picker.Item label="Choose category:" value="" />
-              <Picker.Item label="Pants" value="pants" />
-              <Picker.Item label="T-shirts" value="t-shirts" />
-              <Picker.Item label="Socks" value="socks" />
-              <Picker.Item label="Shoes" value="shoes" />
-              <Picker.Item label="Hoodies & Sweatshirts" value="hoodiesAndSweatshirts" />
-              <Picker.Item label="Dresses" value="dresses" />
-            </Picker>
-          </View>
+        <View style={{ height: 50, marginBottom: 15, }}>
+          <Text style={{ alignSelf: "center" }}>Title of product:</Text>
+          <TextInput
+            label='Title'
+            // placeholder='title of product'
+            value={this.state.title}
+            onChangeText={title => this.setState({ title })}
+            style={styles.textInputBorder}
+            underlineColorAndroid='transparent'
+          />
+        </View>
 
-          <View style={{ height: 50, marginBottom: 15, }}>
-            <Text style={{ alignSelf: "center" }}>Color:</Text>
-            <TextInput
-              label='color'
-              // placeholder='color of product'
-              value={this.state.color}
-              onChangeText={color => this.setState({ color })}
-              style={styles.textInputBorder}
-              underlineColorAndroid='transparent'
-            />
-          </View>
+        <View style={{ height: 50, marginBottom: 15, }}>
+          <Text style={{ alignSelf: "center" }}>Brand:</Text>
+          <TextInput
+            label='brand'
+            // placeholder='brand of product'
+            value={this.state.brand}
+            onChangeText={brand => this.setState({ brand })}
+            style={styles.textInputBorder}
+            underlineColorAndroid='transparent'
+          />
+        </View>
 
-          <View style={{ height: 50, marginBottom: 15, }}>
-            <Text style={{ alignSelf: "center" }}>Size:</Text>
-            <TextInput
-              label='size'
-              // placeholder='size of product'
-              value={this.state.size}
-              onChangeText={size => this.setState({ size })}
-              style={styles.textInputBorder}
-              underlineColorAndroid='transparent'
-            />
-          </View>
+        <View style={{ height: 50, marginBottom: 0, }}>
+          <Text style={{ alignSelf: "center" }}>Category:</Text>
+          {/* En "dropdown" picker der kun giver brugere mulighed for at vælge de kategorier vi har oprettet */}
+          <Picker
+            selectedValue={this.state.category}
+            onValueChange={itemValue => this.setState({ category: itemValue })}
+            style={{ marginTop: -15 }}
+          >
+            <Picker.Item label="Choose category:" value="" />
+            <Picker.Item label="Pants" value="pants" />
+            <Picker.Item label="T-shirts" value="t-shirts" />
+            <Picker.Item label="Socks" value="socks" />
+            <Picker.Item label="Shoes" value="shoes" />
+            <Picker.Item label="Hoodies & Sweatshirts" value="hoodiesAndSweatshirts" />
+            <Picker.Item label="Dresses" value="dresses" />
+          </Picker>
+        </View>
 
-          <View style={{ height: 50, marginBottom: 15, }}>
-            <Text style={{ alignSelf: "center" }}>Description:</Text>
-            <TextInput
-              label='description'
-              // placeholder='description of product'
-              value={this.state.description}
-              onChangeText={description => this.setState({ description })}
-              style={styles.textInputBorder}
-              underlineColorAndroid='transparent'
-            />
-          </View>
+        <View style={{ height: 50, marginBottom: 15, }}>
+          <Text style={{ alignSelf: "center" }}>Color:</Text>
+          <TextInput
+            label='color'
+            // placeholder='color of product'
+            value={this.state.color}
+            onChangeText={color => this.setState({ color })}
+            style={styles.textInputBorder}
+            underlineColorAndroid='transparent'
+          />
+        </View>
 
-          <View style={{ flexDirection: "column", height: 50, justifyContent: "space-between", }}>
+        <View style={{ height: 50, marginBottom: 15, }}>
+          <Text style={{ alignSelf: "center" }}>Size:</Text>
+          <TextInput
+            label='size'
+            // placeholder='size of product'
+            value={this.state.size}
+            onChangeText={size => this.setState({ size })}
+            style={styles.textInputBorder}
+            underlineColorAndroid='transparent'
+          />
+        </View>
 
-            <Button title="Take picture of item..." onPress={this.onChooseImagePress} />
-            <Text>{'\n'}</Text>
-            <Button
-              onPress={() => this.addItemToDatabase()}
-              title='Add item to store'
-              color='#2B8144'
-            >
-            </Button>
-          </View>
+        <View style={{ height: 50, marginBottom: 15, }}>
+          <Text style={{ alignSelf: "center" }}>Description:</Text>
+          <TextInput
+            label='description'
+            // placeholder='description of product'
+            value={this.state.description}
+            onChangeText={description => this.setState({ description })}
+            style={styles.textInputBorder}
+            underlineColorAndroid='transparent'
+          />
+        </View>
 
-      </ScrollView>
+        <View style={{ flexDirection: "column", height: 50, justifyContent: "space-between", }}>
+
+          <Button title="Take picture of item..." onPress={this.onChooseImagePress} />
+          <Text>{'\n'}</Text>
+          <Button
+            onPress={() => this.addItemToDatabase()}
+            title='Add item to store'
+            color='#2B8144'
+          >
+          </Button>
+        </View>
+
+      </View>
 
     );
   }
