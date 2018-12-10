7ddbbe988d1d05f0ffd6abd22e6e36d99283eace
luwe16ab@student.cbs.dk <lucasweje@gmail.com>
2018-12-10 13:13:57 +0100

    Tilføjet tekst i et ScrollView om hvordan appen virker

--- components/pages/AboutScreen.js
+++ components/pages/AboutScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, View, Image, Text, StyleSheet } from 'react-native';
+import { ActivityIndicator, View, ScrollView, Image, Text, StyleSheet } from 'react-native';
 import firebase from 'firebase';
 
 
@@ -7,7 +7,7 @@ export default class AboutScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false
+      isLoading: true
     }
   }
 
@@ -16,6 +16,9 @@ export default class AboutScreen extends React.Component {
   };
 
   componentDidMount() {
+    this.setState({
+      isLoading: false,
+    })
   }
 
 
@@ -29,11 +32,48 @@ export default class AboutScreen extends React.Component {
     }
 
     return (
-      <View style={styles.container}>
-        <Text style={{fontSize: 28}}>About Mynewold</Text>
-        <Text>Hej Iben, dette er ABOUT screen :)</Text>
+      <ScrollView style={styles.container}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>About mynewold</Text>
 
-      </View>
+        <View style={styles.AndetView}>
+
+          <Text style={{ fontSize: 18, textDecorationLine: "underline" }}>We are</Text>
+          <Text>
+            We are a start-up of 3 friends based in Copenhagen.
+            {"\n"}{"\n"}We want to make it possible to refresh your wardrobe without spending money or using resources that affect the enviroment.
+            {"\n"}{"\n"}Lorem ipsum dolor sit amet, ipsum volutpat libero, justo ultricies amet. Vehicula semper sed suspendisse eleifend tempor, elit id est. Magna fames aliquam ut, morbi montes in, eu at lorem duis cras velit, sit augue. Scelerisque suscipit, aliquet nec nullam enim integer.
+            {"\n"}{"\n"}Lorem ipsum dolor sit amet, ipsum volutpat libero, justo ultricies amet. Vehicula semper sed suspendisse eleifend tempor, elit id est. Magna fames aliquam ut, morbi montes in, eu at lorem duis cras velit, sit augue. Scelerisque suscipit, aliquet nec nullam enim integer.
+          </Text>
+
+        </View>
+
+        <View style={styles.AndetView}>
+
+          <Text style={{ fontSize: 18, textDecorationLine: "underline"}}>How it works</Text>
+          <Text>Step 1:{"\n"}
+            Add items of clothing you no longer want or have a need for to the market.
+          </Text>
+          <Text>{"\n"}Step 2:{"\n"}
+            When they have been recieved by a 'mynewold' warehouse and made it's way through our quality control,
+            our algorithm will determine the amount of points the item is worth, which will be added to your account.
+          </Text>
+          <Text>{"\n"}Step 3:{"\n"}
+            Browse through the market and spend your points on clothing other people have added to the market.
+          </Text>
+
+        </View>
+
+        <View style={styles.AndetView}>
+
+          <Text style={{ fontSize: 18, textDecorationLine: "underline" }}>Payment and Delivery</Text>
+          <Text>
+            Lorem ipsum dolor sit amet, ipsum volutpat libero, justo ultricies amet. Vehicula semper sed suspendisse eleifend tempor, elit id est. Magna fames aliquam ut, morbi montes in, eu at lorem duis cras velit, sit augue. Scelerisque suscipit, aliquet nec nullam enim integer.
+            {"\n"}{"\n"}Lorem ipsum dolor sit amet, ipsum volutpat libero, justo ultricies amet. Vehicula semper sed suspendisse eleifend tempor, elit id est. Magna fames aliquam ut, morbi montes in, eu at lorem duis cras velit, sit augue. Scelerisque suscipit, aliquet nec nullam enim integer.
+          </Text>
+
+        </View>
+
+      </ScrollView>
 
     );
   }
@@ -43,15 +83,17 @@ export default class AboutScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    marginTop: 40,
+    paddingRight: 15,
+    marginRight: 5,
+    marginLeft: 20,
+
   },
-  imageStyle: {
-    resizeMode: 'contain',
-    width: 300,
-    height: 400,
-    marginLeft: 25,
-    marginRight: 25,
-    borderRadius: 10,
+  AndetView: {
+    marginBottom: 10,
+    marginTop: 20,
+    marginLeft: 10,
+    flexDirection: 'column'
   },
 });
